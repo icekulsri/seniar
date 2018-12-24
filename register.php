7836d69a0d9cff27edc3e76c42f6bdd96ac71508
icekulsri <nattanitk1997@gmail.com>
2018-12-24 15:42:06 +0700

    initial commit

---
+++ register.php
@@ -0,0 +1,16 @@
+<html>
+<link href="register.css" rel="stylesheet">
+    <head>
+
+    </head>
+    <body>
+    <header>
+    <span>โครงการพี่สอนน้อง</span>
+        <form>
+          <tr>
+              <td><input type="submit" value="เข้าสู่ระบบ"</td>
+          </tr>
+        </form>
+    </header>
+    </body>
+</html>
